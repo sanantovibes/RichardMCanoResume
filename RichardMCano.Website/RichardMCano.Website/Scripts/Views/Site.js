18dc3b0c1fea3ec1c1b42e156c617a0d3c82b18f
sanantovibes <richardmcano@gmail.com>
2017-03-27 22:48:32 -0500

    Fixed the base URL by adding a "/" to the end

--- RichardMCano.Website/RichardMCano.Website/Scripts/Views/Site.js
+++ RichardMCano.Website/RichardMCano.Website/Scripts/Views/Site.js
@@ -1,6 +1,6 @@
 ﻿////var baseUrl = "http://www.richardmcano.com/";
 //var baseUrl = "http://localhost:62403/";
-var baseUrl = location.protocol + "//" + location.host;
+var baseUrl = location.protocol + "//" + location.host + "/";
 var id = "6257B7B5-C4D0-4D00-ACB4-350A95861B7F";
 
 $(document).ready(function () {
