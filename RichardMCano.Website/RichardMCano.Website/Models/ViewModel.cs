50f4ea53d5ced4c672ed95ca8a08acecc31e604a
sanantovibes <richardmcano@gmail.com>
2017-03-27 23:41:31 -0500

    Added Footer partial with DateTime.Now.Year.ToString() to get current year for footer

--- RichardMCano.Website/RichardMCano.Website/Models/ViewModel.cs
+++ RichardMCano.Website/RichardMCano.Website/Models/ViewModel.cs
@@ -8,10 +8,12 @@ namespace RichardMCano.Website.Models
     public class ViewModel
     {
         public string Title { get; set; }
+        public string Year { get; set; }
 
         public ViewModel()
         {
             Title = "Richard M. Cano Rèsumè";
+            Year = DateTime.Now.Year.ToString();
         }
     }
 }
